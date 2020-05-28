704a2d9f2c7e5e6d891f5960e1be570ed56caf59
Robin Long <robin.long1@hotmail.co.uk>
2020-05-28 09:21:25 +0100

    Adding versioning variables to build-all-dockerfiles.sh to make changing versions easier

--- build-all-dockerfiles.sh
+++ build-all-dockerfiles.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-version=2020.2
-
-for simd in SSE2 AVX_256 AVX2_256 AVX_512
+gromacs_version=2020.2
+cuda_version=10.2
+simd_types=(SSE2 AVX_256 AVX2_256 AVX_512)
+for simd in ${simd_types[@]}
 do
-    tag=gmx-$version-cuda-10.2-$simd
+    tag=gmx-$gromacs_version-cuda-$cuda_version-$simd
     mkdir -p $tag
-    python3 build-dockerfiles.py --simd $simd --gcc 8 --cuda 10.2 --version $version --format docker > $tag/Dockerfile
+    python3 build-dockerfiles.py --simd $simd --gcc 8 --cuda $cuda_version --version $gromacs_version --format docker > $tag/Dockerfile
 done
